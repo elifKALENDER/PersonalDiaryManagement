376c2ab9d0459a585a492087393636d6f19e1db6
kalender <elifkalender.tr@gmail.com>
2023-10-11 01:08:38 +0300

    First Commit

---
+++ PersonalDiaryManagementSystem-dll/day.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalDiaryManagementSystem_dll {
+    public static class day {
+
+        public static string FindDay( int InpYear,int InpMonth,int InpDay) {
+
+            DateTime myDate = new DateTime(InpYear, InpMonth, InpDay);
+            string dayName = myDate.DayOfWeek.ToString();
+
+            return dayName;
+        }
+
+        
+        public static string SetId( int personId,  int date ) {
+
+           //TO DO
+           return(personId + "_" + date);
+        }
+    }
+}
